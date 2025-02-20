189c3fa262fcf3b99ec8e466f44030f8baaf9221
Dpgytbot <155309888+Dpgytbot@users.noreply.github.com>
2025-02-20 22:54:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,16 +1,16 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
+  API_ID = int(os.environ.get("API_ID", "602443041994"))
   API_HASH = os.environ.get("API_HASH", "e53eecba602443041994af39d029f3f6")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6437947811:AAETguqBfMr-R0vN4QqFLsXp4j-q1D-x2uw")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "6437947811:AAETguqBfMr-R0vN4QqFLsXp4j-q1D-x2uw")
   DB_CHANNEL = int(os.environ.get("db url", "mongodb+srv://<dpdev0>:<1234dp>@cluster0.gonun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://<dpdev0>:<1234dp>@cluster0.gonun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "indianblackhat47")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
